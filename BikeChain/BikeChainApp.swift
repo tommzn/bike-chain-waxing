73297559d0ed8e66897bcdaf52dce1b187438a6a
Thomas Schenker <tommzn@gmx.de>
2026-03-30 14:57:18 +0200

    Initial Commit

---
+++ BikeChain/BikeChainApp.swift
@@ -0,0 +1,32 @@
+//
+//  BikeChainApp.swift
+//  BikeChain
+//
+//  Created by Thomas Schenker on 30.03.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BikeChainApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
